691dd832989ec15414594f55835e96549f1f912d
DeepakPandey <deepakgpandey05@gmail.com>
2025-07-09 11:29:59 +0530

    updated code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://deepakPandey:9827678994@cluster0.hsaie49.mongodb.net/food-del')
+  await mongoose.connect(process.env.MONGO_URI)
+ // await mongoose.connect('mongodb+srv://deepakPandey:9827678994@cluster0.hsaie49.mongodb.net/food-del')
   .then(() => {
     console.log("DB Connected");
   });
